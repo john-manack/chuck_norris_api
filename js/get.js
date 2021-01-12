8a7d24a6cd5ad87d86195ff41984bf484ecfaf18
John Manack <jmnck28@gmail.com>
2021-01-12 12:08:52 -0500

    add styling

--- js/get.js
+++ js/get.js
@@ -1,12 +1,15 @@
-// Step 1: Ask for the data
-fetch('https://api.chucknorris.io/jokes/random?category=dev')
+'use strict';
+
+// YOU CAN USE THIS FUNCTION BELOW FOR ANY API THAT USES JSON
+function get(url) {
+    // Step 1: Ask for the data
+    return fetch(url)
     // Step 2: get a response, ask for the response for the JSON, return the JSON
     .then(function (response) {
         return response.json();
     })
     // Step 3: Do Stuff with the data from the JSON
     .then(data => {
-        updateBody(data.value);
         return data;
     })
     // Step 4: Make sure any errors are flagged.
@@ -14,3 +17,4 @@ fetch('https://api.chucknorris.io/jokes/random?category=dev')
         console.error("ERROR:", error);
         return error;
     });
+};
--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
 'use strict';
 
+function getQuote() {
+    const url = "https://api.chucknorris.io/jokes/random?category=dev";
+    get(url).then(response => {
+        updateBody(response.value);
+    });
+}
+
+function getCategories() {
+    const url = "https://api.chucknorris.io/jokes/categories";
+    get(url).then(response => {
+        console.log("Category response:", response);
+        buildCategoryList(response);
+    });
+}
+
 function updateBody(quote) {
     const main = document.querySelector('#main');
     // use the 'createElement' method to make a new element in the DOM
@@ -8,3 +23,19 @@ function updateBody(quote) {
     // This actually injects the paragraph into the DOM
     main.appendChild(paragraph);
 }
+
+// Create a list of the categories from the API and place it in the DOM.
+function buildCategoryList(categoryList) {
+    const main = document.querySelector('#main')
+    const categoryUL = document.createElement('ul');
+    categoryList.map(category => {
+        const categoryLI = document.createElement('li');
+        categoryLI.innerHTML = category;
+        categoryUL.appendChild(categoryLI);
+    });
+    console.log(categoryUL);
+    main.appendChild(categoryUL);
+}
+
+getCategories();
+getQuote();
